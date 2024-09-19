e95c7ddebb6a9e8ee5be4dc7d3839b2d27576be5
harrystark74 <dragonxyz.2020@gmail.com>
2024-09-20 05:51:22 +0700

    Add files via upload

---
+++ robot_nckh.ino
@@ -0,0 +1,522 @@
+//khai báo các thư viện
+#include <Servo.h>    
+#include "FlexiTimer2.h"
+#include "SoftwareSerial.h"
+SoftwareSerial HC05(0,1);// khai báo chân RX,TX của bluetooth HC05
+char data = 0;
+char dieu_khien;
+Servo servo[4][3]; //khai báo mảng servo
+const int servo_pin[4][3] = { {2, 3, 4}, {5, 6, 7}, {8, 9, 10}, {11, 12, 13} };
+const float length_a = 55; //kích thước của robot
+const float length_b = 77.5;
+const float length_c = 27.5;
+const float length_side = 71;
+const float z_absolute = -28;
+const float z_default = -50, z_up = -30, z_boot = z_absolute;
+const float x_default = 62, x_offset = 0;
+const float y_start = 0, y_step = 40;
+const float y_default = x_default;
+volatile float site_now[4][3];    
+volatile float site_expect[4][3]; 
+float temp_speed[4][3];  
+float move_speed;     
+float speed_multiple = 1; 
+const float spot_turn_speed = 4;
+const float leg_move_speed = 8;
+const float body_move_speed = 3;
+const float stand_seat_speed = 1;
+volatile int rest_counter;      
+const float KEEP = 255;
+const float pi = 3.1415926;
+const float temp_a = sqrt(pow(2 * x_default + length_side, 2) + pow(y_step, 2));
+const float temp_b = 2 * (y_start + y_step) + length_side;
+const float temp_c = sqrt(pow(2 * x_default + length_side, 2) + pow(2 * y_start + y_step + length_side, 2));
+const float temp_alpha = acos((pow(temp_a, 2) + pow(temp_b, 2) - pow(temp_c, 2)) / 2 / temp_a / temp_b);
+const float turn_x1 = (temp_a - length_side) / 2;
+const float turn_y1 = y_start + y_step / 2;
+const float turn_x0 = turn_x1 - temp_b * cos(temp_alpha);
+const float turn_y0 = temp_b * sin(temp_alpha) - turn_y1 - length_side;
+void set_site(int leg, float x, float y, float z)
+{
+  float length_x = 0, length_y = 0, length_z = 0;
+
+  if (x != KEEP)
+    length_x = x - site_now[leg][0];
+  if (y != KEEP)
+    length_y = y - site_now[leg][1];
+  if (z != KEEP)
+    length_z = z - site_now[leg][2];
+
+  float length = sqrt(pow(length_x, 2) + pow(length_y, 2) + pow(length_z, 2));
+
+  temp_speed[leg][0] = length_x / length * move_speed * speed_multiple;
+  temp_speed[leg][1] = length_y / length * move_speed * speed_multiple;
+  temp_speed[leg][2] = length_z / length * move_speed * speed_multiple;
+
+  if (x != KEEP)
+    site_expect[leg][0] = x;
+  if (y != KEEP)
+    site_expect[leg][1] = y;
+  if (z != KEEP)
+    site_expect[leg][2] = z;
+}
+//hàm wait_reach khi servo tới điểm chỉ định
+void wait_reach(int leg)
+{
+  while (1)
+    if (site_now[leg][0] == site_expect[leg][0])
+      if (site_now[leg][1] == site_expect[leg][1])
+        if (site_now[leg][2] == site_expect[leg][2])
+          break;
+}
+//hàm wait_all_reach khi tất cả servo cung tới vị trí chỉ định
+void wait_all_reach(void)
+{
+  for (int i = 0; i < 4; i++)
+    wait_reach(i);
+}
+//hàm chuyển đổi tọa độ(x,y) sang tọa độ bán kính và góc
+void cartesian_to_polar(volatile float &alpha, volatile float &beta, volatile float &gamma, volatile float x, volatile float y, volatile float z)
+{
+  float v, w;
+  w = (x >= 0 ? 1 : -1) * (sqrt(pow(x, 2) + pow(y, 2)));
+  v = w - length_c;
+  alpha = atan2(z, v) + acos((pow(length_a, 2) - pow(length_b, 2) + pow(v, 2) + pow(z, 2)) / 2 / length_a / sqrt(pow(v, 2) + pow(z, 2)));
+  beta = acos((pow(length_a, 2) + pow(length_b, 2) - pow(v, 2) - pow(z, 2)) / 2 / length_a / length_b);
+  gamma = (w >= 0) ? atan2(y, x) : atan2(-y, -x);
+  alpha = alpha / pi * 180;
+  beta = beta / pi * 180;
+  gamma = gamma / pi * 180;
+}
+//thay đổi góc của servo dựa trên bán kính và góc
+void polar_to_servo(int leg, float alpha, float beta, float gamma)
+{
+  if (leg == 0)
+  {
+    alpha = 90 - alpha;
+    beta = beta;
+    gamma += 90;
+  }
+  else if (leg == 1)
+  {
+    alpha += 90;
+    beta = 180 - beta;
+    gamma = 90 - gamma;
+  }
+  else if (leg == 2)
+  {
+    alpha += 90;
+    beta = 180 - beta;
+    gamma = 90 - gamma;
+  }
+  else if (leg == 3)
+  {
+    alpha = 90 - alpha;
+    beta = beta;
+    gamma += 90;
+  }
+
+  servo[leg][0].write(alpha);
+  servo[leg][1].write(beta);
+  servo[leg][2].write(gamma);
+}
+//hàm yêu cầu của servo
+void servo_service(void)
+{
+  sei();
+  static float alpha, beta, gamma;
+
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      if (abs(site_now[i][j] - site_expect[i][j]) >= abs(temp_speed[i][j]))
+        site_now[i][j] += temp_speed[i][j];
+      else
+        site_now[i][j] = site_expect[i][j];
+    }
+
+    cartesian_to_polar(alpha, beta, gamma, site_now[i][0], site_now[i][1], site_now[i][2]);
+    polar_to_servo(i, alpha, beta, gamma);
+  }
+
+  rest_counter++;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Robot starts initialization");
+  set_site(0, x_default - x_offset, y_start + y_step, z_boot);
+  set_site(1, x_default - x_offset, y_start + y_step, z_boot);
+  set_site(2, x_default + x_offset, y_start, z_boot);
+  set_site(3, x_default + x_offset, y_start, z_boot);
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      site_now[i][j] = site_expect[i][j];
+    }
+  }
+  //bắt đầu yêu cầu cho các servo
+  FlexiTimer2::set(20, servo_service);
+  FlexiTimer2::start();
+  Serial.println("Servo service started");
+  //khởi tạo các servos
+  servo_attach();
+  Serial.println("Servos initialized");
+  Serial.println("Robot initialization Complete");
+  HC05.begin(9600);
+}
+//hàm gán mảng servo
+void servo_attach(void)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      servo[i][j].attach(servo_pin[i][j]);
+      delay(100);
+    }
+  }
+}
+
+void servo_detach(void)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      servo[i][j].detach();
+      delay(100);
+    }
+  }
+}
+//hàm để robot đứng
+void stand()
+{
+  move_speed = stand_seat_speed;
+  for (int leg = 0; leg < 4; leg++)
+  {
+    set_site(leg, KEEP, KEEP, z_default);
+  }
+  wait_all_reach();
+}
+//hàm để robot quay bên trái
+void turn_left()
+{
+    move_speed = spot_turn_speed;
+    if (site_now[3][1] == y_start)
+    {
+      //leg 3&1 move
+      set_site(3, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+
+      set_site(0, turn_x1 - x_offset, turn_y1, z_default);
+      set_site(1, turn_x0 - x_offset, turn_y0, z_default);
+      set_site(2, turn_x1 + x_offset, turn_y1, z_default);
+      set_site(3, turn_x0 + x_offset, turn_y0, z_up);
+      wait_all_reach();
+
+      set_site(3, turn_x0 + x_offset, turn_y0, z_default);
+      wait_all_reach();
+
+      set_site(0, turn_x1 + x_offset, turn_y1, z_default);
+      set_site(1, turn_x0 + x_offset, turn_y0, z_default);
+      set_site(2, turn_x1 - x_offset, turn_y1, z_default);
+      set_site(3, turn_x0 - x_offset, turn_y0, z_default);
+      wait_all_reach();
+
+      set_site(1, turn_x0 + x_offset, turn_y0, z_up);
+      wait_all_reach();
+
+      set_site(0, x_default + x_offset, y_start, z_default);
+      set_site(1, x_default + x_offset, y_start, z_up);
+      set_site(2, x_default - x_offset, y_start + y_step, z_default);
+      set_site(3, x_default - x_offset, y_start + y_step, z_default);
+      wait_all_reach();
+
+      set_site(1, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
+    else
+    {
+      //leg 0&2 move
+      set_site(0, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+
+      set_site(0, turn_x0 + x_offset, turn_y0, z_up);
+      set_site(1, turn_x1 + x_offset, turn_y1, z_default);
+      set_site(2, turn_x0 - x_offset, turn_y0, z_default);
+      set_site(3, turn_x1 - x_offset, turn_y1, z_default);
+      wait_all_reach();
+
+      set_site(0, turn_x0 + x_offset, turn_y0, z_default);
+      wait_all_reach();
+
+      set_site(0, turn_x0 - x_offset, turn_y0, z_default);
+      set_site(1, turn_x1 - x_offset, turn_y1, z_default);
+      set_site(2, turn_x0 + x_offset, turn_y0, z_default);
+      set_site(3, turn_x1 + x_offset, turn_y1, z_default);
+      wait_all_reach();
+
+      set_site(2, turn_x0 + x_offset, turn_y0, z_up);
+      wait_all_reach();
+
+      set_site(0, x_default - x_offset, y_start + y_step, z_default);
+      set_site(1, x_default - x_offset, y_start + y_step, z_default);
+      set_site(2, x_default + x_offset, y_start, z_up);
+      set_site(3, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+
+      set_site(2, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
+}
+//hàm robot quay bên phải
+void turn_right()
+{
+    move_speed = spot_turn_speed;
+
+    if (site_now[2][1] == y_start)
+    {
+      //leg 2&0 move
+      set_site(2, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+
+      set_site(0, turn_x0 - x_offset, turn_y0, z_default);
+      set_site(1, turn_x1 - x_offset, turn_y1, z_default);
+      set_site(2, turn_x0 + x_offset, turn_y0, z_up);
+      set_site(3, turn_x1 + x_offset, turn_y1, z_default);
+      wait_all_reach();
+
+      set_site(2, turn_x0 + x_offset, turn_y0, z_default);
+      wait_all_reach();
+
+      set_site(0, turn_x0 + x_offset, turn_y0, z_default);
+      set_site(1, turn_x1 + x_offset, turn_y1, z_default);
+      set_site(2, turn_x0 - x_offset, turn_y0, z_default);
+      set_site(3, turn_x1 - x_offset, turn_y1, z_default);
+      wait_all_reach();
+
+      set_site(0, turn_x0 + x_offset, turn_y0, z_up);
+      wait_all_reach();
+
+      set_site(0, x_default + x_offset, y_start, z_up);
+      set_site(1, x_default + x_offset, y_start, z_default);
+      set_site(2, x_default - x_offset, y_start + y_step, z_default);
+      set_site(3, x_default - x_offset, y_start + y_step, z_default);
+      wait_all_reach();
+
+      set_site(0, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
+    else
+    {
+      //leg 1&3 move
+      set_site(1, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+
+      set_site(0, turn_x1 + x_offset, turn_y1, z_default);
+      set_site(1, turn_x0 + x_offset, turn_y0, z_up);
+      set_site(2, turn_x1 - x_offset, turn_y1, z_default);
+      set_site(3, turn_x0 - x_offset, turn_y0, z_default);
+      wait_all_reach();
+
+      set_site(1, turn_x0 + x_offset, turn_y0, z_default);
+      wait_all_reach();
+
+      set_site(0, turn_x1 - x_offset, turn_y1, z_default);
+      set_site(1, turn_x0 - x_offset, turn_y0, z_default);
+      set_site(2, turn_x1 + x_offset, turn_y1, z_default);
+      set_site(3, turn_x0 + x_offset, turn_y0, z_default);
+      wait_all_reach();
+
+      set_site(3, turn_x0 + x_offset, turn_y0, z_up);
+      wait_all_reach();
+
+      set_site(0, x_default - x_offset, y_start + y_step, z_default);
+      set_site(1, x_default - x_offset, y_start + y_step, z_default);
+      set_site(2, x_default + x_offset, y_start, z_default);
+      set_site(3, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+
+      set_site(3, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
+}
+//hàm tiến tới
+void step_forward()
+{
+    move_speed = leg_move_speed;
+
+    if (site_now[2][1] == y_start)
+    {
+      //leg 2&1 move
+      set_site(2, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(2, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(2, x_default + x_offset, y_start + 2 * y_step, z_default);
+      wait_all_reach();
+
+      move_speed = body_move_speed;
+
+      set_site(0, x_default + x_offset, y_start, z_default);
+      set_site(1, x_default + x_offset, y_start + 2 * y_step, z_default);
+      set_site(2, x_default - x_offset, y_start + y_step, z_default);
+      set_site(3, x_default - x_offset, y_start + y_step, z_default);
+      wait_all_reach();
+
+      move_speed = leg_move_speed;
+
+      set_site(1, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(1, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(1, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
+    else
+    {
+      //leg 0&3 move
+      set_site(0, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(0, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(0, x_default + x_offset, y_start + 2 * y_step, z_default);
+      wait_all_reach();
+
+      move_speed = body_move_speed;
+
+      set_site(0, x_default - x_offset, y_start + y_step, z_default);
+      set_site(1, x_default - x_offset, y_start + y_step, z_default);
+      set_site(2, x_default + x_offset, y_start, z_default);
+      set_site(3, x_default + x_offset, y_start + 2 * y_step, z_default);
+      wait_all_reach();
+
+      move_speed = leg_move_speed;
+
+      set_site(3, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(3, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(3, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
+}
+//hàm lui lại
+void step_back()
+{
+    move_speed = leg_move_speed;
+    if (site_now[3][1] == y_start)
+    {
+      //leg 3&0 move
+      set_site(3, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(3, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(3, x_default + x_offset, y_start + 2 * y_step, z_default);
+      wait_all_reach();
+
+      move_speed = body_move_speed;
+
+      set_site(0, x_default + x_offset, y_start + 2 * y_step, z_default);
+      set_site(1, x_default + x_offset, y_start, z_default);
+      set_site(2, x_default - x_offset, y_start + y_step, z_default);
+      set_site(3, x_default - x_offset, y_start + y_step, z_default);
+      wait_all_reach();
+
+      move_speed = leg_move_speed;
+
+      set_site(0, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(0, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(0, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
+    else
+    {
+      //leg 1&2 move
+      set_site(1, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(1, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(1, x_default + x_offset, y_start + 2 * y_step, z_default);
+      wait_all_reach();
+
+      move_speed = body_move_speed;
+
+      set_site(0, x_default - x_offset, y_start + y_step, z_default);
+      set_site(1, x_default - x_offset, y_start + y_step, z_default);
+      set_site(2, x_default + x_offset, y_start + 2 * y_step, z_default);
+      set_site(3, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+
+      move_speed = leg_move_speed;
+
+      set_site(2, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(2, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(2, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
+}
+
+void body_left(int i)
+{
+  set_site(0, site_now[0][0] + i, KEEP, KEEP);
+  set_site(1, site_now[1][0] + i, KEEP, KEEP);
+  set_site(2, site_now[2][0] - i, KEEP, KEEP);
+  set_site(3, site_now[3][0] - i, KEEP, KEEP);
+  wait_all_reach();
+}
+
+void body_right(int i)
+{
+  set_site(0, site_now[0][0] - i, KEEP, KEEP);
+  set_site(1, site_now[1][0] - i, KEEP, KEEP);
+  set_site(2, site_now[2][0] + i, KEEP, KEEP);
+  set_site(3, site_now[3][0] + i, KEEP, KEEP);
+  wait_all_reach();
+}
+
+//hàm điều khiển chính thông qua bluetooth
+void loop()
+{
+  
+  if (HC05.available() > 0) 
+  {
+    dieu_khien = HC05.read();
+    Serial.println(dieu_khien);
+    switch (dieu_khien)//điều khiển robot di chuyển dựa trên các phím trên ứng dụng Bluetooth RC Controller   
+    {
+      case 'F':
+         Serial.println("Step forward");
+         step_forward();
+         break;
+      case 'B':
+         Serial.println("Step back");
+         step_back();
+         break;   
+      case 'L':       
+         Serial.println("Turn left");
+         turn_left();
+         break;
+      case 'R':
+          Serial.println("Turn right");
+          turn_right();
+          break;
+      case 'X':
+         Serial.println("Stand");
+         stand();
+         break;
+   }
+}
+
+}
+
+
+
