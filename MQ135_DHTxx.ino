fe05aad34c3c85b4926d392f02aad9c2633fd315
harrystark74 <dragonxyz.2020@gmail.com>
2024-09-20 12:25:37 +0700

    Update MQ135_DHTxx.ino

--- MQ135_DHTxx.ino
+++ MQ135_DHTxx.ino
@@ -1,50 +1,39 @@
-#include <MQ135.h>
+#include <MQ135.h>//khai báo thư viện
 #include <DHT.h>
-
-/* MQ135 + DHT Temp Sensor
-
-   Combination of the MQ135 air quality sensor and a DHT11/22 temperature sensor to accurately measure ppm values through the library correction.
-   Uses the Adafruit DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-
-   Written by: https://github.com/NuclearPhoenixx/MQ135
-*/
-
-#define PIN_MQ135 A1 // MQ135 Analog Input Pin
-#define DHTPIN A0 // DHT Digital Input Pin
-#define DHTTYPE DHT11 // DHT11 or DHT22, depends on your sensor
+#define PIN_MQ135 A1 // khai báo các chân của MQ135 và DHT11
+#define DHTPIN A0 // 
+#define DHTTYPE DHT11 // 
 
 MQ135 mq135_sensor(PIN_MQ135);
 DHT dht(DHTPIN, DHTTYPE);
 
-float temperature, humidity; // Temp and Humid floats, will be measured by the DHT
+float temperature, humidity; 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);//bắt đầu cổng kết nối
 
   dht.begin();
 }
 
 void loop() {
-  humidity = dht.readHumidity();
-  temperature = dht.readTemperature();
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(humidity) || isnan(temperature)) {
+  humidity = dht.readHumidity();// đọc giá trị độ ẩm từ DHT11
+  temperature = dht.readTemperature();//đọc giá trị nhiệt độ của DHT11
+  if (isnan(humidity) || isnan(temperature)) {   //kiểm tra lỗi của chương trình
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
-
+  //lấy dữ liệu từ cảm biến mq135
   float rzero = mq135_sensor.getRZero();
-  float correctedRZero = mq135_sensor.getCorrectedRZero(temperature, humidity);
+  float correctedRZero = mq135_sensor.getCorrectedRZero(temperature, humidity); //cảm biến MQ135 sẽ lấy dữ liệu từ DHT11 để tăng độ chính xác
   float resistance = mq135_sensor.getResistance();
   float ppm = mq135_sensor.getPPM();
   float correctedPPM = mq135_sensor.getCorrectedPPM(temperature, humidity);
 
-  Serial.print("MQ135 RZero: ");
+  Serial.print("MQ135 RZero: ");//in ra giá trị của MQ135
   Serial.print(rzero);
   Serial.print("\t Corrected RZero: ");
   Serial.print(correctedRZero);
-  Serial.print("\t Resistance: ");
+  Serial.print("\t Resistance: ");// giá trị điện trở
   Serial.print(resistance);
   Serial.print("\t PPM: ");
   Serial.print(ppm);
